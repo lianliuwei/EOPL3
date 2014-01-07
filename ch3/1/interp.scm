3c3a80f65d1e79c9d66b9f2db6d7a0eec24e14d8
lianliuwei <lianliuwei@gmail.com>
2014-01-07 22:53:56 +0800

    impl e3.6

---
+++ ch3/1/interp.scm
@@ -0,0 +1,17 @@
+(module interp (lib "eopl.ss" "eopl")
+  
+  ;; interpreter for the LET language.  The \commentboxes are the
+  ;; latex code for inserting the rules into the code in the book.
+  ;; These are too complicated to put here, see the text, sorry.
+
+  (require "drscheme-init.scm")
+
+  (require "lang.scm")
+  (require "data-structures.scm")
+  (require "environments.scm")
+  
+  ;(provide value-of-program value-of)
+
+
+)
+
