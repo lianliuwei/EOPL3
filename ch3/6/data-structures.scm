d4e0a411b8c6f87257a8a9ac824e4df639b28f55
lianliuwei <lianliuwei@gmail.com>
2014-01-30 21:15:01 +0800

    implement 3.15

--- ch3/6/data-structures.scm
+++ ch3/6/data-structures.scm
@@ -27,6 +27,12 @@
   (define empty-list-val
     (list-val '()))
   
+  (define list->list-val
+    (lambda (list) 
+      (if (null? list)
+          empty-list-val
+          (list-val (list->list-val-help list)))))
+  
   (define list->list-val-help
     (lambda (list)
       (if (null? list)
@@ -38,11 +44,20 @@
              (list? (car list) (list->list-val (car list))))
            (list->list-val-help (cdr list))))))
   
-  (define list->list-val
-    (lambda (list) 
-      (if (null? list)
-          empty-list-val
-          (list-val (list->list-val-help list)))))
+  (define expval->val
+    (lambda (exp)
+      (cases expval exp
+        (num-val (val) val)
+        (bool-val (val) val)
+        (list-val (val) (expval->list-help val))
+        )))
+  
+  (define expval->list-help
+    (lambda (list-exp)
+      (if (null? list-exp)
+          '()
+          (cons (expval->val (car list-exp))
+                (expval->list-help (cdr list-exp))))))
   
   ;;; extractors:
   
--- ch3/6/main.scm
+++ ch3/6/main.scm
@@ -67,8 +67,8 @@
                 (run (cadr test))))
           (else (eopl:error 'run-one "no such test: ~s" test-name))))))
   
-
-   (define the-lexical-spec
+  
+  (define the-lexical-spec
     '((whitespace (whitespace) skip)
       (comment ("%" (arbno (not #\newline))) skip)
       (identifier
@@ -80,30 +80,30 @@
   
   (define the-grammar
     '((program (expression) a-program)
-
+      
       (expression 
        (number) 
        const-exp)
-
+      
       (expression 
        (identifier) 
        var-exp)
       
       (expression
-        ("-" "(" expression "," expression ")")
-        diff-exp)
+       ("-" "(" expression "," expression ")")
+       diff-exp)
       
       (expression 
-        ("+" "(" expression "," expression ")") 
-        add-exp)
+       ("+" "(" expression "," expression ")") 
+       add-exp)
       
       (expression
-        ("*" "(" expression "," expression ")") 
-        mulit-exp)
+       ("*" "(" expression "," expression ")") 
+       mulit-exp)
       
       (expression
-        ("quotient" "(" expression "," expression ")") 
-        quotient-exp)
+       ("quotient" "(" expression "," expression ")") 
+       quotient-exp)
       
       (expression 
        ("minus" "(" expression ")") 
@@ -112,49 +112,55 @@
       (expression
        ("if" bool-expression "then" expression "else" expression)
        if-exp)
-
+      
       (expression
        ("let" identifier "=" expression "in" expression)
        let-exp)   
       
       (expression 
-        ("cons" "(" expression "," expression ")")
-        cons-exp)
+       ("cons" "(" expression "," expression ")")
+       cons-exp)
       
       (expression
-        ("car" "(" expression ")")
-        car-exp)
+       ("car" "(" expression ")")
+       car-exp)
       
       (expression
-        ("cdr" "(" expression ")")
-        cdr-exp)
+       ("cdr" "(" expression ")")
+       cdr-exp)
       
       (expression
-        ("emptylist")
-        emptylist-exp)
+       ("emptylist")
+       emptylist-exp)
       
       (expression
-        ("list" "(" (separated-list expression ",") ")")
-        list-exp)
+       ("list" "(" (separated-list expression ",") ")")
+       list-exp)
       
       (expression
-        ("cond"  (arbno "{" bool-expression "==>" expression "}") "end")
-        cond-exp)
+       ("cond"  (arbno "{" bool-expression "==>" expression "}") "end")
+       cond-exp)
       
-      (expression (bool-expression) a-bool-expression)
+      (expression 
+       ("print" "(" expression ")")
+       print-exp)
+      
+      (expression 
+       (bool-expression) 
+       a-bool-expression)
       
       ;; bool-expression
       (bool-expression 
-        ("equal?" "(" expression "," expression ")")
-        equal?-exp)
+       ("equal?" "(" expression "," expression ")")
+       equal?-exp)
       
       (bool-expression 
-        ("greater?" "(" expression "," expression ")")
-        greater?-exp)
+       ("greater?" "(" expression "," expression ")")
+       greater?-exp)
       
       (bool-expression
-        ("less?" "(" expression "," expression ")")
-        less?-exp)
+       ("less?" "(" expression "," expression ")")
+       less?-exp)
       
       (bool-expression
        ("zero?" "(" expression ")") 
@@ -174,7 +180,7 @@
   (define just-scan
     (sllgen:make-string-scanner the-lexical-spec the-grammar))
   
-
+  
   (define value-of-program
     (lambda (exp)
       (cases program exp
@@ -184,7 +190,7 @@
     (lambda (exp env)
       (cases expression exp
         (const-exp (exp1) (num-val exp1))
- 
+        
         (a-bool-expression (exp1) (value-of-bool-expression exp1 env))
         
         (diff-exp (exp1 exp2) 
@@ -193,14 +199,14 @@
                             (expval->num (value-of exp2 env)))))
         
         (add-exp (exp1 exp2) 
-                  (num-val (+ 
-                            (expval->num (value-of exp1 env)) 
-                            (expval->num (value-of exp2 env)))))
+                 (num-val (+ 
+                           (expval->num (value-of exp1 env)) 
+                           (expval->num (value-of exp2 env)))))
         
         (mulit-exp (exp1 exp2) 
-                  (num-val (* 
-                            (expval->num (value-of exp1 env)) 
-                            (expval->num (value-of exp2 env)))))
+                   (num-val (* 
+                             (expval->num (value-of exp1 env)) 
+                             (expval->num (value-of exp2 env)))))
         
         (quotient-exp (exp1 exp2) 
                       (num-val (quotient 
@@ -211,8 +217,8 @@
         (var-exp (exp1) (apply-env env exp1))
         
         (if-exp (exp1 exp2 exp3) (if (expval->bool (value-of-bool-expression exp1 env)) 
-                                                  (value-of exp2 env)
-                                                  (value-of exp3 env)))
+                                     (value-of exp2 env)
+                                     (value-of exp3 env)))
         
         (minus-exp (exp1) (num-val (- 0 (expval->num (value-of exp1 env)))))
         
@@ -231,37 +237,45 @@
                                               (expval->list (value-of exp2 env)))))
         
         (cond-exp (exps1 exps2) (value-of-cond exps1 exps2 env))
+        
+        (print-exp (exp1) (value-of-print (value-of exp1 env)))
+        
         )))
   
   (define value-of-bool-expression
     (lambda (exp env)
       (cases bool-expression exp
         (equal?-exp (exp1 exp2) 
-                      (bool-val (equal? 
-                                (expval->num (value-of exp1 env)) 
-                                (expval->num (value-of exp2 env)))))
+                    (bool-val (equal? 
+                               (expval->num (value-of exp1 env)) 
+                               (expval->num (value-of exp2 env)))))
         
         (greater?-exp (exp1 exp2) 
                       (bool-val (>
-                                (expval->num (value-of exp1 env)) 
-                                (expval->num (value-of exp2 env)))))
+                                 (expval->num (value-of exp1 env)) 
+                                 (expval->num (value-of exp2 env)))))
         
         (less?-exp (exp1 exp2) 
-                      (bool-val (< 
-                                (expval->num (value-of exp1 env)) 
-                                (expval->num (value-of exp2 env)))))
+                   (bool-val (< 
+                              (expval->num (value-of exp1 env)) 
+                              (expval->num (value-of exp2 env)))))
         
         (zero?-exp (exp1) (bool-val (= (expval->num (value-of exp1 env)) 0)))
         
         )))
   
+  (define value-of-print
+    (lambda (expval)
+      (eopl:printf "print ~s~%" (expval->val expval))
+      (num-val 1)))
+   
   (define value-of-cond
     (lambda (conds results env)
       (if (null? conds)
-       (eopl:error 'value-of-cond "no one branch is true")
-       (if (expval->bool (value-of-bool-expression (car conds) env))
-           (value-of (car results) env)
-           (value-of-cond (cdr conds) (cdr results) env)))))
+          (eopl:error 'value-of-cond "no one branch is true")
+          (if (expval->bool (value-of-bool-expression (car conds) env))
+              (value-of (car results) env)
+              (value-of-cond (cdr conds) (cdr results) env)))))
   
   (define value-of-exps
     (lambda (exps env)
@@ -272,16 +286,16 @@
   
   (define test-list
     '(
-  
+      
       ;; simple arithmetic
       (positive-const "11" 11)
       (negative-const "-33" -33)
       (simple-arith-1 "-(44,33)" 11)
-  
+      
       ;; nested arithmetic
       (nested-arith-left "-(-(44,33),22)" -11)
       (nested-arith-right "-(55, -(22,11))" 44)
-  
+      
       ;; simple variables
       (test-var-1 "x" 10)
       (test-var-2 "-(x,1)" 9)
@@ -290,7 +304,7 @@
       ;; simple unbound variables
       (test-unbound-var-1 "foo" error)
       (test-unbound-var-2 "-(x,foo)" error)
-  
+      
       ;; simple conditionals
       (if-true "if zero?(0) then 3 else 4" 3)
       (if-false "if zero?(1) then 3 else 4" 4)
@@ -299,7 +313,7 @@
       (no-bool-to-diff-1 "-(zero?(0),1)" error)
       (no-bool-to-diff-2 "-(1,zero?(0))" error)
       (no-int-to-if "if 1 then 2 else 3" error)
-
+      
       ;; make sure that the test and both arms get evaluated
       ;; properly. 
       (if-eval-test-true "if zero?(-(11,11)) then 3 else 4" 3)
@@ -308,19 +322,19 @@
       ;; and make sure the other arm doesn't get evaluated.
       (if-eval-test-true-2 "if zero?(-(11, 11)) then 3 else foo" 3)
       (if-eval-test-false-2 "if zero?(-(11,12)) then foo else 4" 4)
-
+      
       ;; simple let
       (simple-let-1 "let x = 3 in x" 3)
-
+      
       ;; make sure the body and rhs get evaluated
       (eval-let-body "let x = 3 in -(x,1)" 2)
       (eval-let-rhs "let x = -(4,1) in -(x,1)" 2)
-
+      
       ;; check nested let and shadowing
       (simple-nested-let "let x = 3 in let y = 4 in -(x,y)" -1)
       (check-shadowing-in-body "let x = 3 in let x = 4 in x" 4)
       (check-shadowing-in-rhs "let x = 3 in let x = -(x,1) in x" 2)
-
+      
       ;; test minus operate
       (just-minus "minus(-1)" 1)
       (book-exam "minus(-(minus(5), 9))" 14)
@@ -360,10 +374,13 @@
       (simple-cond "cond{zero?(1) ==> 1} {zero?(0) ==> 2} end" 2)
       (two-true-cond "cond{zero?(0) ==> 1} {zero?(0) ==> 2} {equal?(10,1) ==> 3} end" 1)
       ;;(cond-in-let "let t = zero?(0) in let z = zero?(1) in cond {t ==> 1} {z ==> 2} end" 1)
+      
+      ; print operate
+      (printf "print(cons(1,cons(zero?(0),cons(cons(10,emptylist),emptylist))))" 1)
       ))
   
   (run-all)
-  ;; 96
+  ;; pdf 97
   )
 
 
